d83e7e68777b4aa4f6e02a2bb44d2a25db967d31
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-06-12 13:07:45 +0300

    firebase uninstall

--- src/components/info_page/InfoPage.js
+++ src/components/info_page/InfoPage.js
@@ -264,15 +264,7 @@ function InfoPage() {
                     <button onClick={submitForm}> Submit </button>
                 </div>
             </div>
-
         </main>
-
-        {/*<footer>*/}
-        {/*    <iframe*/}
-        {/*        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3510.0865901784077!2d24.02859087296597!3d49.837825641728514!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473add64f46130af%3A0xb8fa7f852817fcdc!2z0JTQstC-0YDQtdGGINCf0L7RgtC-0YbQutC40YU!5e0!3m2!1sru!2sua!4v1683615861353!5m2!1sru!2sua"*/}
-        {/*        width="600" height="450" allowFullScreen="" loading="lazy"*/}
-        {/*        referrerPolicy="no-referrer-when-downgrade"></iframe>*/}
-        {/*</footer>*/}
         <Contacts/>
 
     </div>);
